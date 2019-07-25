4ffec9d7dff18dade1f6b88c0667c291cb26722a
Mir47 <zvm6568@naver.com>
2019-07-26 01:55:43 +0900

    century 출력 고민중 2

--- visual/test/ConsoleApp1/ConsoleApp1/Program.cs
+++ visual/test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,26 +11,16 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int century=0;
-            century = centuryFromYear(1);
+            century = centuryFromYear(123);
              Console.WriteLine(century);           
             Console.ReadKey();
             //오늘은 여기까지? 네 네
         }
         static int centuryFromYear(int year)
         {
-            if (year >= 1 && year <= 99)
-            {
-
-            }
-            if (year >= 100 && year <= 999)
-            {
-
-            }
-            if (year >= 1000 && year <= 2005)
-            {
-
-            }
-            return 0;
+            int century = 0;
+            century = year / 100 + 1;
+            return century;
         }
     }
 }
