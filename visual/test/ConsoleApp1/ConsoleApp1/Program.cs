8707cb82282e0d015e9380c749e04237285ec559
Mir47 <zvm6568@naver.com>
2019-07-26 01:44:23 +0900

    century 함수 고민중

--- visual/test/ConsoleApp1/ConsoleApp1/Program.cs
+++ visual/test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,12 +9,28 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        { 
-
-             Console.WriteLine("Hello World!");           
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit."); 
+        {
+            int century=0;
+            century = centuryFromYear(1);
+             Console.WriteLine(century);           
             Console.ReadKey();
+            //오늘은 여기까지? 네 네
+        }
+        static int centuryFromYear(int year)
+        {
+            if (year >= 1 && year <= 99)
+            {
+
+            }
+            if (year >= 100 && year <= 999)
+            {
+
+            }
+            if (year >= 1000 && year <= 2005)
+            {
+
+            }
+            return 0;
         }
     }
 }
