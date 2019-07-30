2c07bdb4710233f300cf4fcfa8ee89da9e5a2beb
Mir47 <zvm6568@naver.com>
2019-07-30 12:42:52 +0900

    정리

--- visual/test/ConsoleApp1/ConsoleApp1/Program.cs
+++ visual/test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,17 +10,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            char Palindrome;
-            Palindrome = checkPalindrome(aabaa);
-             Console.WriteLine(Palindrome);           
+            char palindrome;
+            palindrome = checkPalindrome(aabaa);
+             Console.WriteLine(palindrome);           
             Console.ReadKey();
         }
         static bool checkPalindrome(string inputString)
         {
-            bool checkPalindrome(string inputString);//if문으로 만약 글자수가 짝수면 양쪽에서 순서대로 비교
-            //짝수가 아니면 가운데를 제외한 양쪽에서 순서대로 비교
-            char Palindrome;
-             return Palindrome;
+            bool result = false;
+            //코드 짜야됌
+            return result;
         }
     }
 } 
