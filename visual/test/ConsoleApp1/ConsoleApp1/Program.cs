532b8a7833dc84b3f9a54f46c4ae10740c2b0d0d
Mir47 <zvm6568@naver.com>
2019-08-06 15:00:19 +0900

    s1, s2대신 직접 써넣음 오류는 없지만 뭐가 문제일까요? 61번줄 인덱스가 벗어났다고 나와요

--- visual/test/ConsoleApp1/ConsoleApp1/Program.cs
+++ visual/test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,31 +16,72 @@ namespace ConsoleApp1
         }
         static bool checkPalindrome(string inputString)
         {
-            bool result = false;
+            bool result = true;
             //검사 로직 구현
             String checkPalindrome = "aabaa";
-            int count = checkPalindrome.Length;
-            int/* a=1,b=0,*/count1;
+            int count = checkPalindrome.Length;// count에 checkPalindrome의 문자 갯수를 세어 넣음
+            int a=1, b=0, count1;
             /*while (a==count)
             {
-                char s%a = checkPalindrome[b];
+                char s = checkPalindrome[b];
                 a++;
+                b++;
             }
-            보류*/
             char s1 = checkPalindrome[0];
             char s2 = checkPalindrome[1];
             char s3 = checkPalindrome[2];
             char s4 = checkPalindrome[3];
             char s5 = checkPalindrome[4];
-            //char s6 = checkPalindrome[5];
+            //char s6 = checkPalindrome[5];*/
             count1 = count % 2;
-            if (count!=0&&count1==0 ) //짝수라면
+           if(count != 0 && count1 == 0)
             {
-                /*if (s1 == s6)
+                a = 1; b = 0;
+                while (a <= count1)
+                {
+                    if (checkPalindrome[a - 1] == checkPalindrome[count - b])
+                    {
+
+                    }
+                    else
+                    {
+                        result= false;
+                    }
+                    a++;
+                    b++;
+                }
+            }
+            else
+            {
+                if (count != 0)
+                {
+                    a = 1; b = 0;
+                    while (a <= count1)
+                    {
+                        if (checkPalindrome[a - 1] == checkPalindrome[count - b])
+                        {
+
+                        }
+                        else
+                        {
+                            result = false;
+                        }
+                        a++;
+                        b++;
+                    }
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            /*if (count!=0&&count1==0 ) //짝수라면
+            {
+                if (s1 == s6)
                 {
                     if (s2 == s5)
                     {
-                        if(s3==s4)
+                        if (s3 == s4)
                         {
 
                         }
@@ -57,9 +98,9 @@ namespace ConsoleApp1
                 else
                 {
                     result = false;
-                */
+                }
             }
-            /*Console.WriteLine("짝수다");*/
+            /*Console.WriteLine("짝수다");*
         
             else//짝수가 아니면
             {
@@ -80,8 +121,8 @@ namespace ConsoleApp1
                 {
                     result = false;
                 }
-                /*Console.WriteLine("홀수다");*/
-            }
+                //Console.WriteLine("홀수다");
+            }*/
             return result;//result를 반환
         }
     }
